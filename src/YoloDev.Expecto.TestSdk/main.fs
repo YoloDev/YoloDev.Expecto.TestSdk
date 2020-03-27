0518ae25d011f0e6d86fe98e630f476402e80bf9
Aleksander Heintz <alxandr@alxandr.me>
2020-03-27 19:43:37 +0100

    Workflow

--- src/YoloDev.Expecto.TestSdk/main.fs
+++ src/YoloDev.Expecto.TestSdk/main.fs
@@ -1,6 +1,6 @@
 module YoloDev.Expecto.TestSdk.Main
 
-#if NETCOREAPP2_0
+#if NETCOREAPP2_1
 [<EntryPoint>]
 let main _ = failwithf "YoloDev.Expecto.TestSdk is not intended to be run as a program"
 #endif
