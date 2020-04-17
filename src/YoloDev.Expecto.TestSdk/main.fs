859ae6c904697a3f5e10d44008af8aedeb5936df
Michał Niegrzybowski <mniegrzybowski@datto.com>
2020-04-17 08:08:26 +0200

    update to .net core 3.1

--- src/YoloDev.Expecto.TestSdk/main.fs
+++ src/YoloDev.Expecto.TestSdk/main.fs
@@ -1,6 +1,6 @@
 module YoloDev.Expecto.TestSdk.Main
 
-#if NETCOREAPP2_1
+#if NETCOREAPP3_1
 [<EntryPoint>]
 let main _ = failwithf "YoloDev.Expecto.TestSdk is not intended to be run as a program"
 #endif
